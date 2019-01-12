5655f0e867da81420aeee16603bb6f7b0a1f9e34
lovdeepm <lovdeep@gmail.com>
2019-01-12 11:45:03 -0800

    second

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+
+var randomNumber = [];
+var crystal1 = [];
+var crystal2 = [];
+var crystal3 = [];
+var crystal4 = [];
+
+
+var wins = 0;
+var loss = 0;
+var totalScore = [];
+
+
+for (i = 0; i < 4; i++){
+    randomNumber = Math.floor(Math.random() *12);
+    console.log(randomNumber)
+
+    
+}
+
+
+
